cc7eff730a8247501130b4983cae5c37e077aefc
Freeman-Nkosi <44724054+Freeman-Nkosi@users.noreply.github.com>
2019-07-02 16:36:24 +0200

    Add files via upload

---
+++ JavaScript/Box.js
@@ -0,0 +1,34 @@
+function green(Main){
+	Main.style.backgroundColor = "green";
+	var g=71;
+	var e=69;
+	var n=78;
+	var r=82;
+	
+	Main.innerHTML="Hi, my name is Green";
+	Main.innerHTML+="<br> Calculatoins: G=71 + double E=69 + N=78 +R=82: " ;
+	var Answer=g+e+e+n+r;
+Main.innerHTML+=Answer;
+}
+function blue(Main){
+	var b=66;
+	var l=76;
+	var u=85;
+	var e=69;
+	Main.style.backgroundColor="blue";
+	Main.innerHTML="Hi, my name is Blue";
+	Main.innerHTML+="<br> Calculatoins: B=66 + L=76 + U=85 + E=69: ";
+	var Answer=b+l+u+e;
+	Main.innerHTML+=b+l+u+e;
+}
+
+function red(Main){
+Main.style.backgroundColor="red";
+var r=82;
+var e=69;
+var d=68;
+Main.innerHTML="Hi,my name is Red";
+Main.innerHTML+="<br> Calculatoins: R=82 +E=69 +D=68: " ;
+var Answer= r + e+ d;
+Main.innerHTML+=Answer;
+}
